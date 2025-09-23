0fd6eb231a29f196e6373e9a17a1a3a94fd9797c
Addison Huschka <yg0531qj@go.minnstate.edu>
2025-09-23 11:13:07 -0500

    Create C# input codes.cs

---
+++ Week 5/C# input codes.cs
@@ -0,0 +1,14 @@
+using System;
+namespace CSharp
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string name = "Addison";
+			Console.WriteLine(name);
+			Console.Write("Hello World! ");
+			Console.Write("Welcome to CIS 121");
+		}
+	}
+}
